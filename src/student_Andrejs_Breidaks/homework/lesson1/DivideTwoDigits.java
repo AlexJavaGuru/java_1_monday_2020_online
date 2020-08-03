e661a98cf2fdd1f3244e2db36a0ad302d5e7228c
AndrejsB86 <andrejs.breidaks@inbox.lv>
2020-08-03 19:28:37 +0300

    Homework lesson 1 + test

---
+++ src/student_Andrejs_Breidaks/homework/lesson1/DivideTwoDigits.java
@@ -0,0 +1,10 @@
+package student_Andrejs_Breidaks.homework.lesson1;
+
+public class DivideTwoDigits {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 3;
+        System.out.println(x / y);
+    }
+}
+
---
+++ src/student_Andrejs_Breidaks/homework/lesson1/SumTwoDigits.java
@@ -0,0 +1,9 @@
+package student_Andrejs_Breidaks.homework.lesson1;
+
+public class SumTwoDigits {
+    public static void main(String[] args) {
+        int x = 74;
+        int y = 36;
+        System.out.println(x + y);
+    }
+}
