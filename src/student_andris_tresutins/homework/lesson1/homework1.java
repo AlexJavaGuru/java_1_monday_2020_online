7352412f99bbd1d469e5963903eccc660c02b1d8
Darxney <tresutinsandris@gmail.com>
2020-08-03 20:01:19 +0300

    Lesson1 every Homework

--- src/student_andris_tresutins/homework/lesson1/homework1.java
+++ src/student_andris_tresutins/homework/lesson1/homework1.java
@@ -1,12 +1,10 @@
 package student_andris_tresutins.homework.lesson1;
 
 public class homework1 {
-
     public static void main(String[] args) {
         int sum, div;
         sum= 74+36;
         div= 50/3;
         System.out.println(" Hello \n Andris Tresutins \n sum: "+sum+"\n div: "+div);
     }
-
 }
