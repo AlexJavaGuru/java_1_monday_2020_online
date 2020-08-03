5a8e96dfab7103f5f11987aec0cc87695c3c6f4f
Edgar Edessey <edgarzbu4@gmail.com>
2020-08-03 21:23:00 +0300

    Github lecture 2 java class

--- src/student_edgar_edessey/homework/lesson_1/Sum.java
+++ src/student_edgar_edessey/homework/lesson_1/Sum.java
@@ -2,8 +2,9 @@ package student_edgar_edessey.homework.lesson_1;
 
 public class Sum {
     public static void main(String[] args) {
-        int num1 = 74, num2 = 36, sum;
-        sum = num1 + num2;
+        int num1 = 74;
+        int num2 = 36;
+        int sum = num1 + num2;
         System.out.println("Sum of these numbers: " + sum);
     }
 }
