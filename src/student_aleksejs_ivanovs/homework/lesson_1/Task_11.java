be6ed29a41663bd67c162159339b622190204713
GhostSpudy <iv.aleksejs@inbox.lv>
2020-08-03 19:39:27 +0300

    Github commit

---
+++ src/student_aleksejs_ivanovs/homework/lesson_1/Task_11.java
@@ -0,0 +1,8 @@
+package student_aleksejs_ivanovs.homework.lesson_1;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        System.out.println(74+36);
+        System.out.println(50/3);
+    }
+}
