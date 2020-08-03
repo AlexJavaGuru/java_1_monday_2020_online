0c7e53d1c23698693a646dbe9601741f93a05a36
Dmbej <dmbej@hot.ee>
2020-08-03 20:21:30 +0300

    Commit Test v2

---
+++ src/student_dmitry_vasiliev/Hello.java
@@ -0,0 +1,10 @@
+package student_dmitry_vasiliev;
+
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+    }
+}
+
---
+++ src/student_dmitry_vasiliev/Plus.java
@@ -0,0 +1,9 @@
+package student_dmitry_vasiliev;
+
+public class Plus {
+    public static void main(String[] args){
+        int a = 74;
+        int b = 36;
+        int c = a + b;
+        System.out.println(c);}
+}
