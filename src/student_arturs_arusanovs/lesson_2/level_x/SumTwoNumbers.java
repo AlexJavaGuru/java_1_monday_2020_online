7d4135f14bb6d738d7641213c6cebd40f98fb6b6
Arturs Arusanovs <sollnet@inbox.lv>
2020-08-09 19:21:13 +0300

    Homework

---
+++ src/student_arturs_arusanovs/lesson_2/level_x/SumTwoNumbers.java
@@ -0,0 +1,13 @@
+package student_arturs_arusanovs.lesson_2.level_x;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        int numberOne=10;
+        int numberTwo=20;
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+        int sum = numberOne + numberTwo;
+        System.out.println("Sum = " + sum);
+    }
+
+}
