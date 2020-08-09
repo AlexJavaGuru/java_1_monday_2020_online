dc34dfd8f6cf24de5bbde741c03d3bad9281af31
Dmbej <dmbej@hot.ee>
2020-08-10 00:21:08 +0300

    Commit Lesson_2 Task_18

---
+++ src/student_dmitry_vasiliev/lesson_2/homework/level_1/Task_18.java
@@ -0,0 +1,10 @@
+package student_dmitry_vasiliev.lesson_2.homework.level_1;
+
+public class Task_18 {
+
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+    }
+
