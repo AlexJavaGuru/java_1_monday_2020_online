70db36342dab7e8cc299246a16463a3d9457499d
Arturs Arusanovs <sollnet@inbox.lv>
2020-08-09 18:38:43 +0300

    Homework

---
+++ src/student_arturs_arusanovs/lesson_2/level_5/SizeMatters.java
@@ -0,0 +1,8 @@
+package student_arturs_arusanovs.lesson_2.level_5;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
