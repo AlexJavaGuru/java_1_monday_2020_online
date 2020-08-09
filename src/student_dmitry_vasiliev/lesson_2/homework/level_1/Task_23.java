ced115742fdb9dba980eaec1d0fd7f3b931b3122
Dmbej <dmbej@hot.ee>
2020-08-10 00:33:41 +0300

    Commit Lesson_2 Task_23

---
+++ src/student_dmitry_vasiliev/lesson_2/homework/level_1/Task_23.java
@@ -0,0 +1,11 @@
+package student_dmitry_vasiliev.lesson_2.homework.level_1;
+
+public class Task_23 {
+
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+
+
+
+    }
+}
