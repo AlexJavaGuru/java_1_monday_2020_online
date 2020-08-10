db444dbf0f3a14dc9e815873b08d6a3e40e73479
lilija <lgj@inbox.lv>
2020-08-11 02:53:54 +0300

    homework lesson 1

---
+++ src/student_lilija_g/homework/lesson_1/level_4/Task_10.java
@@ -0,0 +1,8 @@
+package student_lilija_g.homework.lesson_1.level_4;
+
+public class Task_10 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Lilija Graholska");
+    }
+}
