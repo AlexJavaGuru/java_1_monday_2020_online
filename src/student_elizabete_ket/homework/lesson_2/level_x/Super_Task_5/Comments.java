c48ade525f6e8b94912931b4b292f7ef32caa809
Elizabete-jg <eljza78@inbox.lv>
2020-08-10 16:32:17 +0300

    Completed level_x task 5

---
+++ src/student_elizabete_ket/homework/lesson_2/level_x/Super_Task_5/Comments.java
@@ -0,0 +1,11 @@
+package student_elizabete_ket.homework.lesson_2.level_x.Super_Task_5;
+
+public class Comments {
+    public static void main (String [] args) {
+        //vivesti na ekran celoe chislo 89
+        int number = 89;
+                /* eto moja popitka
+                prodemonstrirovatj mnogostrochnij komment */
+        System.out.println(number);
+    }
+}
