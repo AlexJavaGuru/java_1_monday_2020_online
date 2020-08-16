c06b20d8fce51a2f9c56ce109d883bacd9968642
Alexander <nortropltd@gmail.com>
2020-08-17 00:45:55 +0300

    Homework lesson1 level4

---
+++ src/student_alexander_shl/homework/lesson_1/level_4/Task_10.java
@@ -0,0 +1,8 @@
+package student_alexander_shl.homework.lesson_1.level_4;
+
+public class Task_10 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Alexander Shlarko");
+    }
+}
