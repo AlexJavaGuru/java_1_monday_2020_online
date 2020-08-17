ec051f0a77f1f7301ce298ce1dadd99f114ea7bc
Darxney <tresutinsandris@gmail.com>
2020-08-17 20:16:00 +0300

    Completed Lesson 4 Level 1

---
+++ src/student_andris_tresutins/homework/lesson_4/level_1/Task_1.java
@@ -0,0 +1,17 @@
+package student_andris_tresutins.homework.lesson_4.level_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        int number = -1;
+
+        if (number > 0) {
+
+            System.out.println(number + " - number is positive");
+
+        } else {
+
+            System.out.println(number + " - number is negative");
+
+        }
+    }
+}
---
+++ src/student_andris_tresutins/homework/lesson_4/level_1/Task_2.java
@@ -0,0 +1,21 @@
+package student_andris_tresutins.homework.lesson_4.level_1;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int number = 0;
+
+        if( number > 0 ){
+
+            System.out.println(number + " - number is positive");
+
+        } else if ( number == 0) {
+
+            System.out.println(number + " - number is not negative or positive");
+
+        }
+        else {
+            System.out.println(number + " - number is negative");
+        }
+    }
+
+}
