442eeb8f15318870f49da8c3b982c91fd3d5412e
Darxney <tresutinsandris@gmail.com>
2020-08-17 20:34:45 +0300

    Completed Lesson 4 Level 2

---
+++ src/student_andris_tresutins/homework/lesson_4/level_2/Task_6.java
@@ -0,0 +1,22 @@
+package student_andris_tresutins.homework.lesson_4.level_2;
+
+import java.util.Scanner;
+
+public class Task_6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println( "enter number 1");
+        int num1 = input.nextInt();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println( "enter number 2");
+        int num2 = input2.nextInt();
+
+        if (num1 < num2){
+            System.out.println(num1 + " is smaller");
+        }
+        else{
+            System.out.println(num2 + " is smaller");
+        }
+    }
+}
