ebc19bb152792b70330752ef3238d0fbece31f7c
admin <dmbej@hot.ee>
2020-08-18 20:08:27 +0300

    Lesson3_Task_1.java

---
+++ src/student_dmitry_vasiliev/lesson_3/homework/RobotUDemo.java
@@ -0,0 +1,15 @@
+package student_dmitry_vasiliev.lesson_3.homework;
+
+
+public class RobotUDemo {
+
+        public static void main(String[] args) {
+            RobotU myFirstRobot = new RobotU("R1");
+
+
+            myFirstRobot.sayHello();
+            myFirstRobot.sayYourName();
+
+        }
+    }
+
