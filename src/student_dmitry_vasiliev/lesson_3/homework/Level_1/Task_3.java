212195869db1ba8d746ce470b28025c20dd4dc5d
admin <dmbej@hot.ee>
2020-08-18 21:32:24 +0300

    Lesson3_Task_3.java

---
+++ src/student_dmitry_vasiliev/lesson_3/homework/Level_1/Task_3.java
@@ -0,0 +1,10 @@
+package student_dmitry_vasiliev.lesson_3.homework.Level_1;
+
+public class Task_3 {
+    public class RobotUdemo{
+
+    public void main(String[] args) {
+        RobotU Robot = new RobotU();
+    }
+}
+}
