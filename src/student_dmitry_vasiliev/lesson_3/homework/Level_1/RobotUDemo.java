e2262ac7dbc71e4b67c8edbc43a249b7cb2a7602
admin <dmbej@hot.ee>
2020-08-18 23:12:26 +0300

    Lesson3_Task_1-6.java

--- src/student_dmitry_vasiliev/lesson_3/homework/Level_1/RobotUDemo.java
+++ src/student_dmitry_vasiliev/lesson_3/homework/Level_1/RobotUDemo.java
@@ -4,10 +4,25 @@ package student_dmitry_vasiliev.lesson_3.homework.Level_1;
 public class RobotUDemo {
 
         public static void main(String[] args) {
-            RobotU myRobotU = new RobotU("ROBO1");
+            RobotU myRobotU = new RobotU("ROBO");
             myRobotU.sayHello1();
             myRobotU.sayHello();
             myRobotU.sayYourName();
+
+            RobotU myRobotU1 = new RobotU("ROBO1");
+            myRobotU1.sayHello1();
+            myRobotU1.sayHello();
+            myRobotU1.sayYourName();
+
+            RobotU myRobotU2 = new RobotU("ROBO2");
+            myRobotU2.sayHello1();
+            myRobotU2.sayHello();
+            myRobotU2.sayYourName();
+
+            RobotU myRobotU3 = new RobotU("ROBO3");
+            myRobotU3.sayHello1();
+            myRobotU3.sayHello();
+            myRobotU3.sayYourName();
 
         }
     }
