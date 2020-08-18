cc1c04a23601957462f4fd179fdee610971f639a
admin <dmbej@hot.ee>
2020-08-18 23:34:16 +0300

    Lesson3_Task_7-9.java

---
+++ src/student_dmitry_vasiliev/lesson_3/homework/Level_2/StringName.java
@@ -0,0 +1,8 @@
+package student_dmitry_vasiliev.lesson_3.homework.Level_2;
+
+public class StringName {
+    public static void main(String[] args) {
+        String myName = "Dmitri";
+        System.out.print(myName);
+    }
+}
