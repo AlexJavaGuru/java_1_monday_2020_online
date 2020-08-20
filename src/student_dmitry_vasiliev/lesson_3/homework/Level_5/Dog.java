b2b44844e2e5c42780f1d6b9a2d5562ced27b323
admin <dmbej@hot.ee>
2020-08-20 23:15:52 +0300

    Lesson3_Level_4_Task_18-20.java

---
+++ src/student_dmitry_vasiliev/lesson_3/homework/Level_5/Dog.java
@@ -0,0 +1,19 @@
+package student_dmitry_vasiliev.lesson_3.homework.Level_5;
+
+public class Dog {
+    private String name;
+
+    public static void bark() {
+        System.out.println("Wof...");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
