f98d52254e69757972cac70bc3639515a420b8ee
admin <dmbej@hot.ee>
2020-08-22 14:15:20 +0300

    Lesson3_Level_6_Task_26.java

---
+++ src/student_dmitry_vasiliev/lesson_3/homework/Level_6/Name.java
@@ -0,0 +1,32 @@
+package student_dmitry_vasiliev.lesson_3.homework.Level_6;
+
+public class Name {
+    private String firstName;
+    private String secondName;
+    private int age;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public int getAge() {
+        return age;}
+
+}
