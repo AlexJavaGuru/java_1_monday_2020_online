8c6d6c60220cae8fbd35f4499828247b5a44a669
admin <dmbej@hot.ee>
2020-08-23 22:25:34 +0300

    package student_dmitry_vasiliev.lesson_4.level_3.Task_8;

---
+++ src/student_dmitry_vasiliev/lesson_4/level_3/Task_8.java
@@ -0,0 +1,30 @@
+package student_dmitry_vasiliev.lesson_4.level_3;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    private int num;
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter please first integer number...");
+        int num1 = in.nextInt();
+
+        System.out.println("Enter please second integer number...");
+        int num2 = in.nextInt();
+
+        System.out.println("Enter please second integer number...");
+        int num3 = in.nextInt();
+
+        int result1 = num1 - num2;
+        int result2 = num1 - num3;
+        int result3 = num3 - num2;
+        if (result1 == 0 && result2 == 0 && result3 == 0)
+            System.out.println("All numbers are equal.");
+        if (result1 != 0 && result2 != 0 && result3 != 0)
+            System.out.println("All numbers are different.");
+        else
+            System.out.println("Neither all are equal or different.");
+    }
+}
+
