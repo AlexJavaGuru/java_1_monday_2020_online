d5aa48c0d1899003150aedd422056b2315945f4c
admin <dmbej@hot.ee>
2020-08-23 11:56:14 +0300

    Lesson3_Level_6_Task_28.java

---
+++ src/student_dmitry_vasiliev/lesson_3/homework/Level_6/Computer.java
@@ -0,0 +1,18 @@
+package student_dmitry_vasiliev.lesson_3.homework.Level_6;
+
+public class Computer {
+    private String manufacturer;
+    private String model;
+
+    public Computer(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+    public String getModel() {
+        return this.model;
+    }
+
+}
---
+++ src/student_dmitry_vasiliev/lesson_3/homework/Level_6/ComputerDemo.java
@@ -0,0 +1,11 @@
+package student_dmitry_vasiliev.lesson_3.homework.Level_6;
+
+public class ComputerDemo {
+    public static void main(String[] args) {
+        Computer dell = new Computer("Dell", "XXX-1");
+        String manufacturer = dell.getManufacturer();
+        String model = dell.getModel();
+        System.out.println("Computer manufacturer = " + manufacturer);
+        System.out.println("Computer model = " + model);
+    }
+}
