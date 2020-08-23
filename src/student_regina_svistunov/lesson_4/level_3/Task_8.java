9299d1926f1be252c0e9acabf7827edd85826e0a
regina <Shjhi91.reg>
2020-08-24 01:40:05 +0300

    Lesson 4 level homework

---
+++ src/student_regina_svistunov/lesson_4/level_3/Task_8.java
@@ -0,0 +1,34 @@
+package student_regina_svistunov.lesson_4.level_3;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" Write first number please: ");
+        int number1 = scanner.nextInt();
+        System.out.println(" Write second number please: ");
+        int number2 = scanner.nextInt();
+        System.out.println(" Write third number please: ");
+        int number3 = scanner.nextInt();
+
+            if (number1 == number2 && number1 == number3 ){
+                System.out.println("All numbers are equals! ");
+            }
+            else if (number1 != number2 && number1 != number3 ) {
+                System.out.println("All numbers are different! ");
+            }
+            else {
+                System.out.println("Neither all are equal or different! ");
+            }
+        }
+    }
+
+
+
+//    Write a Java program that accepts three numbers and prints:
+//        - "All numbers are equal" if all three numbers are equal,
+//        - "All numbers are different" if all three numbers are different
+//        - "Neither all are equal or different" otherwise.
