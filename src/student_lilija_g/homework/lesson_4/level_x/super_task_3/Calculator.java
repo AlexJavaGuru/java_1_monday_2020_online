aecc5e621724dc539f28b9779e1e7023a3e3728f
lilija <lgj@inbox.lv>
2020-08-24 16:57:44 +0300

    lesson_4 level_x super_task_3

---
+++ src/student_lilija_g/homework/lesson_4/level_x/super_task_3/Calculator.java
@@ -0,0 +1,24 @@
+package student_lilija_g.homework.lesson_4.level_x.super_task_3;
+
+class Calculator {
+        public int sum(int firstNumber, int secondNumber) {
+            return firstNumber + secondNumber;
+        }
+
+        public int sub(int firstNumber, int secondNumber) {
+            return firstNumber - secondNumber;
+        }
+
+        public int mul(int firstNumber, int secondNumber) {
+            return firstNumber * secondNumber;
+        }
+
+        public int div(int firstNumber, int secondNumber) {
+            return firstNumber / secondNumber;
+        }
+
+        public boolean isEven(int number) {
+            return number % 2 == 0;
+        }
+    }
+
