d4c13e60b5f329757a3d79ab3a2f0ee1924bda5c
lilija <lgj@inbox.lv>
2020-08-24 13:55:40 +0300

    lesson_4 level_6

---
+++ src/student_lilija_g/homework/lesson_4/level_6/StockTest.java
@@ -0,0 +1,20 @@
+package student_lilija_g.homework.lesson_4.level_6;
+
+class StockTest {
+
+    public static void main(String[] args) {
+
+        Stock google = new Stock("GOOG", 10.00);
+        String priceInformation = google.getPriceInformation();
+
+        System.out.println(priceInformation);
+
+        google.updatePrice(15);
+        google.updatePrice(7);
+        google.updatePrice(14);
+
+        System.out.println(google.getPriceInformation());
+
+    }
+}
+
