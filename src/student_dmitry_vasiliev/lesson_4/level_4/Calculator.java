82e48a82a9476c1d6a6118300fe95d29b168f5a8
Dmbej <dmbej@hot.ee>
2020-08-25 23:31:52 +0300

    student_dmitry_vasiliev.lesson_4.level_3.Task_11-14;

---
+++ src/student_dmitry_vasiliev/lesson_4/level_4/Calculator.java
@@ -0,0 +1,22 @@
+package student_dmitry_vasiliev.lesson_4.level_4;
+
+public class Calculator {
+
+    public int sum (int firstNumber, int secondNumber) {
+            return firstNumber + secondNumber;
+        }
+    public int sub (int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+    public int dev (int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+    public int mul (int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+    public boolean isEven (int number) {
+        return  (number % 2) == 0;
+        }
+
+    }
+
