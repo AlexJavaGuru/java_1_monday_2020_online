e08f840124a132ab33b35409e372096d7ee80106
Dmbej <dmbej@hot.ee>
2020-08-27 23:15:11 +0300

    student_dmitry_vasiliev.lesson_4.level_4.Task_16

---
+++ src/student_dmitry_vasiliev/lesson_4/level_5/LightColorDetector.java
@@ -0,0 +1,21 @@
+package student_dmitry_vasiliev.lesson_4.level_5;
+
+class LightColorDetector {
+    public static String wave(int waveSize) {
+        if (waveSize >= 380 && waveSize <=449)
+            return "Violet";
+        else if (waveSize >= 450 && waveSize <=494)
+          return "Blue";
+        else if (waveSize >= 495 && waveSize <=569)
+            return "Green";
+        else if (waveSize >= 570 && waveSize <=589)
+            return "Yellow";
+        else if (waveSize >= 590 && waveSize <=619)
+            return "Yellow";
+        else if (waveSize >= 620 && waveSize <=750)
+            return "Yellow";
+        else return "Invisible light";
+    }
+}
+
+
---
+++ src/student_dmitry_vasiliev/lesson_4/level_5/LightColorDetectorTest.java
@@ -0,0 +1,32 @@
+package student_dmitry_vasiliev.lesson_4.level_5;
+
+class LightColorDetectorTest {
+
+    public static void main(String[] args) {
+        LightColorDetectorTest test = new LightColorDetectorTest();
+
+        test.colorTest();
+    }
+        public void colorTest() {
+            int waveSize = 750;
+            LightColorDetector detector = new LightColorDetector();
+            String lightWaveSize = LightColorDetector.wave(waveSize);
+            if ("Violet" == LightColorDetector.wave(waveSize))
+                System.out.println("Test OK. Light is Violet.");
+            else if ("Blue" == LightColorDetector.wave(waveSize))
+                System.out.println("Test OK. Light is Blue.");
+            else if ("Green" == LightColorDetector.wave(waveSize))
+                System.out.println("Test OK. Light is Green.");
+            else if ("Yellow" == LightColorDetector.wave(waveSize))
+                System.out.println("Test OK. Light is Yellow.");
+            else if ("Orange" == LightColorDetector.wave(waveSize))
+                System.out.println("Test OK. Light is Orange.");
+            else if ("Red" == LightColorDetector.wave(waveSize))
+                System.out.println("Test OK. Light is Red.");
+            else if ("Invisible light" == LightColorDetector.wave(waveSize))
+                System.out.println("Test OK. Light is invisible.");
+            else
+                System.out.println("Test not OK.");
+        }
+    }
+
