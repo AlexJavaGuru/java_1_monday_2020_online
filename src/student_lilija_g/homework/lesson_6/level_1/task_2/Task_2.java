8d4ea34d961d68292851d606c212d6aa4a998423
Dmbej <dmbej@hot.ee>
2020-09-07 19:27:10 +0300

    Rollback

--- src/student_lilija_g/homework/lesson_6/level_1/task_2/Task_2.java
+++ src/student_lilija_g/homework/lesson_6/level_1/task_2/Task_2.java
@@ -47,13 +47,13 @@ public class Task_2 {
         int firstNumber = myInput.nextInt();
         if ((firstNumber >=1) && (firstNumber <= 7)) {
             switch (firstNumber) {
-                case 4 -> System.out.println("Thursday");
-                case 3 -> System.out.println("Wednesday");
                 case 1 -> System.out.println("Monday");
-                case 5 -> System.out.println("Friday");
                 case 2 -> System.out.println("Tuesday");
-                case 7 -> System.out.println("Sunday");
+                case 3 -> System.out.println("Wednesday");
+                case 4 -> System.out.println("Thursday");
+                case 5 -> System.out.println("Friday");
                 case 6 -> System.out.println("Saturday");
+                case 7 -> System.out.println("Sunday");
             }
         }
         else {
