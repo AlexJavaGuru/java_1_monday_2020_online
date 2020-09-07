5f5234017f33c2e848e45ba130b61c9e06f5ce58
Dmbej <dmbej@hot.ee>
2020-09-07 19:17:58 +0300

    Тест

--- src/student_lilija_g/homework/lesson_6/level_1/task_2/Task_2.java
+++ src/student_lilija_g/homework/lesson_6/level_1/task_2/Task_2.java
@@ -40,25 +40,25 @@ import java.util.Scanner;
 
 public class Task_2 {
     public static void main(String[] args) {
-        /*
+
         Scanner myInput = new Scanner(System.in);
 
         System.out.print("Input integer number between 1 and 7 : ");
         int firstNumber = myInput.nextInt();
         if ((firstNumber >=1) && (firstNumber <= 7)) {
             switch (firstNumber) {
-                case 1 -> System.out.println("Monday");
-                case 2 -> System.out.println("Tuesday");
-                case 3 -> System.out.println("Wednesday");
                 case 4 -> System.out.println("Thursday");
+                case 3 -> System.out.println("Wednesday");
+                case 1 -> System.out.println("Monday");
                 case 5 -> System.out.println("Friday");
-                case 6 -> System.out.println("Saturday");
+                case 2 -> System.out.println("Tuesday");
                 case 7 -> System.out.println("Sunday");
+                case 6 -> System.out.println("Saturday");
             }
         }
         else {
             System.out.println("Please input a valid number between 1 and 7");
         }
-        */
+
     }
 }
