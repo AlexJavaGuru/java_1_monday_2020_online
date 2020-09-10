7b3be0430d438b40b652cd0c688241eaf613210a
Darxney <tresutinsandris@gmail.com>
2020-09-10 13:50:34 +0300

    Lesson 5 level 5 completed

---
+++ src/student_andris_tresutins/homework/lesson_6/level_2/Task_8.java
@@ -0,0 +1,11 @@
+package student_andris_tresutins.homework.lesson_6.level_2;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
