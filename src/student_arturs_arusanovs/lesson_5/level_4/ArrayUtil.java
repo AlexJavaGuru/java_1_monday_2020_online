3b436ab3e06dcc3bfddcbd98556f03bc3de56ef5
Arturs Arusanovs <sollnet@inbox.lv>
2020-09-11 14:29:14 +0300

    Homework

---
+++ src/student_arturs_arusanovs/lesson_5/level_4/ArrayUtil.java
@@ -0,0 +1,33 @@
+package student_arturs_arusanovs.lesson_5.level_4;
+
+import java.util.Scanner;
+
+class ArrayUtil {
+
+    public int getArrayLengthFromUser() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array length!");
+        int arrLength = sc.nextInt();
+        return  arrLength;
+    }
+
+    public int[] createArray(int arrayLength) {
+        return  new int[arrayLength];
+    }
+
+    public void fillArrayWithNumbersFromUser(int[] arr) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter array element: ");
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    public void printArrayToConsole(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Arr element = " + arr[i]);
+        }
+    }
+
+
+}
