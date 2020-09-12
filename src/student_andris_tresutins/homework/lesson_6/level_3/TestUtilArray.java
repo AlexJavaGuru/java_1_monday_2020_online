515e62a1813f2f9b12029381c53c6c73f1665a38
Darxney <tresutinsandris@gmail.com>
2020-09-12 18:05:02 +0300

    Lesson 6 level 3 completed

--- src/student_andris_tresutins/homework/lesson_6/level_3/TestUtilArray.java
+++ src/student_andris_tresutins/homework/lesson_6/level_3/TestUtilArray.java
@@ -1,9 +1,14 @@
 package student_andris_tresutins.homework.lesson_6.level_3;
+import java.util.Arrays;
 
  class TestUtilArray {
     public static void main(String[] args) {
         TestUtilArray test = new TestUtilArray();
         test.searchnumresult();
+        test.searchnummultipletest();
+        test.replaceNumberTest();
+        test.replaceAllNumbersTest();
+        test.reverseArray();
     }
 
     public void searchnumresult() {
@@ -16,7 +21,74 @@ package student_andris_tresutins.homework.lesson_6.level_3;
         check(expectedResult, actualResult, "Search number in array test");
      }
 
-     public void check(Boolean expectedRes, boolean result, String testName) {
+     public void searchnummultipletest(){
+         UtilArray victim = new UtilArray();
+         int searchNumber = 54;
+
+         int actualresult = victim.numberSearchMultiple(searchNumber);
+         int expectedresult = 3;
+
+         check(expectedresult, actualresult, "Multiple of the same value in array search test");
+     }
+
+     public void replaceNumberTest(){
+         UtilArray victim = new UtilArray();
+         int numbertoreplace = 7;
+         int newNumber = 13;
+
+         int actualresult = victim.replaceNumber(numbertoreplace, newNumber);
+         int expectedresult = 13;
+
+         check(expectedresult,actualresult, "Replace Number in Array test");
+
+     }
+
+     public void replaceAllNumbersTest(){
+         UtilArray victim = new UtilArray();
+         int numberstoreplace = 54;
+         int newNumber = 13;
+
+         boolean actualresult = victim.replaceAllNumbers(numberstoreplace, newNumber).equals("[12, 13, 13, 13, 458]");
+         boolean expectedresult = true;
+
+         check(expectedresult,actualresult, "Replace All specific Numbers in Array test");
+
+     }
+
+     public void reverseArray(){
+         UtilArray victim = new UtilArray();
+
+         boolean actualresult = victim.reverseArray().equals("[458, 14, 54, 13, 12]");
+         boolean expectedresult = true;
+
+         check(expectedresult, actualresult, "Reverse Array test");
+
+
+     }
+
+
+
+     public void check(String expectedRes, String result, String testName) {
+
+         if (expectedRes == result) {
+             System.out.println(testName + " has passed");
+         } else {
+             System.out.println(testName + " failed!");
+             System.out.println("Expected result was : " + expectedRes + " but actual result was : " + result);
+         }
+     }
+
+     public void check(int expectedRes, int result, String testName) {
+
+         if (expectedRes == result) {
+             System.out.println(testName + " has passed");
+         } else {
+             System.out.println(testName + " failed!");
+             System.out.println("Expected result was : " + expectedRes + " but actual result was : " + result);
+         }
+     }
+
+     public void check(boolean expectedRes, boolean result, String testName) {
 
          if (expectedRes == result) {
              System.out.println(testName + " has passed");
--- src/student_andris_tresutins/homework/lesson_6/level_3/UtilArray.java
+++ src/student_andris_tresutins/homework/lesson_6/level_3/UtilArray.java
@@ -1,6 +1,10 @@
 package student_andris_tresutins.homework.lesson_6.level_3;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 class UtilArray {
 
@@ -8,7 +12,6 @@ class UtilArray {
 
      public String numberSearch ( int searchnum) {
 
-         Random randNum = new Random();
 
          int[] Array = new int[5];
 
@@ -41,7 +44,118 @@ class UtilArray {
          }
 
 
-     }
+    public int numberSearchMultiple (int searchnum) {
+
+        int[] Array = new int[5];
+
+        Array[0] = 54;
+        Array[1] = 7;
+        Array[2] = 54;
+        Array[3] = 54;
+        Array[4] = 458;
+
+
+        int i;
+        int foundnumcount = 0;
+        boolean found = false;
+        for (i = 0; i < Array.length; i++) {
+            if (searchnum == Array[i]) {
+                foundnumcount++;
+                found = true;
+            }
+        }
+        if (found) {
+            return foundnumcount;
+        } else {
+            return 0;
+        }
+    }
+
+        public int replaceNumber(int numberToReplace, int newNumber) {
+
+            int[] Array = new int[5];
+
+            Array[0] = 12;
+            Array[1] = 54;
+            Array[2] = 3;
+            Array[3] = 7;
+            Array[4] = 458;
+
+
+            int i;
+            boolean found = false;
+            for ( i = 0; i < Array.length; i++)
+            {
+                if (numberToReplace == Array[i])
+                {
+
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                Array[i] = newNumber;
+                return Array[i];
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+    public String replaceAllNumbers(int numberToReplace, int newNumber) {
+
+        int[] Array = new int[5];
+
+        Array[0] = 12;
+        Array[1] = 54;
+        Array[2] = 54;
+        Array[3] = 54;
+        Array[4] = 458;
+
+
+        int i;
+        boolean found = false;
+        for ( i = 0; i < Array.length; i++)
+        {
+            if (numberToReplace == Array[i])
+            {
+                Array[i] = newNumber;
+                found = true;
+            }
+        }
+        if (found)
+        {
+            return Arrays.toString(Array);
+        }
+        else
+        {
+            return "something went wrong :/";
+        }
+
+    }
+
+
+    public String reverseArray() {
+
+        int[] Array = {12, 13, 54, 14, 458};
+
+        for(int i = 0; i < Array.length / 2; i++)
+        {
+            int arr = Array[i];
+            Array[i] = Array[Array.length - i - 1];
+            Array[Array.length - i - 1] = arr;
+        }
+
+        return Arrays.toString(Array);
+
+    }
+
+
+}
+
 
 
 
