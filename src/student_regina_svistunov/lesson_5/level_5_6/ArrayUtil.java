ef8250a91d84d5eff7555ddd0dd64e3a9703cf47
regina <Shjhi91.reg>
2020-09-14 17:37:18 +0300

    Lesson 5 level 5 and 6 homeworks

---
+++ src/student_regina_svistunov/lesson_5/level_5_6/ArrayUtil.java
@@ -0,0 +1,37 @@
+package student_regina_svistunov.lesson_5.level_5_6;
+
+import java.util.Random;
+
+public class ArrayUtil {
+
+    public int[] createArray(int arrayLength) {
+        int[] resultArray = new int[arrayLength];
+        return resultArray;
+    }
+
+    public void fillArrayWithRandomNumbers(int[] array) {
+        Random random = new Random();
+
+        for (int i=0; i < array.length; i++) {
+            array[i] = random.nextInt(50);
+        }
+    }
+    public int findMaxNumber(int[] array) {
+        int maxNumber = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxNumber) {
+                maxNumber = array[i];
+            }
+            }  return maxNumber;
+    }
+    public int findMinNumber (int[] array) {
+        int minNumber = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minNumber) {
+                minNumber = array[i];
+            }
+        } return minNumber;
+    }
+
+    }
+
