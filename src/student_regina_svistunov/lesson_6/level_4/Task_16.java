b4c937ce43fe491c7f7ad54fc1c9037b130b6c4d
regina <Shjhi91.reg>
2020-09-21 00:01:23 +0300

    Lesson 6 leve 4  homework

---
+++ src/student_regina_svistunov/lesson_6/level_4/Task_16.java
@@ -0,0 +1,24 @@
+package student_regina_svistunov.lesson_6.level_4;
+
+import java.util.Scanner;
+
+public class Task_16 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println(" Please, enter number: ... If enter number 0 you can break. ");
+            int number = scanner.nextInt();
+
+            if (number == 0) {
+                break;
+            }
+        }
+    }
+}
+
+
+    //    Прерывание цикла: «break»
+    //        Обычно цикл завершается при вычислении условия в false.
+    //        Но мы можем выйти из цикла в любой момент с помощью специальной директивы break.
