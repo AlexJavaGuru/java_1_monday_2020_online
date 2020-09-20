cee9504a59008e4ffef3711af30bd84ba0707852
GhostSpudy <iv.aleksejs@inbox.lv>
2020-09-20 20:47:55 +0300

    lesson_6 homework

---
+++ src/student_aleksejs_ivanovs/homework/lesson_6/level_1/task_2/DayOfTheWeekDetectorTest.java
@@ -0,0 +1,85 @@
+package student_aleksejs_ivanovs.homework.lesson_6.level_1.task_2;
+
+class DayOfTheWeekDetectorTest {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetectorTest test = new DayOfTheWeekDetectorTest();
+        test.monday();
+        test.tuesday();
+        test.wednesday();
+        test.thursday();
+        test.friday();
+        test.saturday();
+        test.sunday();
+    }
+
+    public void monday() {
+        DayOfTheWeekDetector day = new DayOfTheWeekDetector();
+        String dayNumber = day.detectDayName(1);
+        if (dayNumber == "Monday") {
+            System.out.println("Ok");
+        } else {
+            System.out.println("Bad");
+        }
+    }
+
+    public void tuesday() {
+        DayOfTheWeekDetector day = new DayOfTheWeekDetector();
+        String dayNumber = day.detectDayName(2);
+        if (dayNumber == "Tuesday") {
+            System.out.println("Ok");
+        } else {
+            System.out.println("Bad");
+        }
+    }
+
+    public void wednesday() {
+        DayOfTheWeekDetector day = new DayOfTheWeekDetector();
+        String dayNumber = day.detectDayName(3);
+        if (dayNumber == "Wednesday") {
+            System.out.println("Ok");
+        } else {
+            System.out.println("Bad");
+        }
+    }
+
+    public void thursday() {
+        DayOfTheWeekDetector day = new DayOfTheWeekDetector();
+        String dayNumber = day.detectDayName(4);
+        if (dayNumber == "Thursday") {
+            System.out.println("Ok");
+        } else {
+            System.out.println("Bad");
+        }
+    }
+
+    public void friday() {
+        DayOfTheWeekDetector day = new DayOfTheWeekDetector();
+        String dayNumber = day.detectDayName(5);
+        if (dayNumber == "Friday") {
+            System.out.println("Ok");
+        } else {
+            System.out.println("Bad");
+        }
+    }
+
+    public void saturday() {
+        DayOfTheWeekDetector day = new DayOfTheWeekDetector();
+        String dayNumber = day.detectDayName(6);
+        if (dayNumber == "Saturday") {
+            System.out.println("Ok");
+        } else {
+            System.out.println("Bad");
+        }
+    }
+
+    public void sunday() {
+        DayOfTheWeekDetector day = new DayOfTheWeekDetector();
+        String dayNumber = day.detectDayName(7);
+        if (dayNumber == "Sunday") {
+            System.out.println("Ok");
+        } else {
+            System.out.println("Bad");
+        }
+    }
+}
