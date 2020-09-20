c40046951a685d3c7ed30e4b6dea56b5a8093f88
regina <Shjhi91.reg>
2020-09-20 21:42:41 +0300

    Lesson 6 level 4  homework

---
+++ src/student_regina_svistunov/lesson_6/level_4/Task_15.java
@@ -0,0 +1,16 @@
+package student_regina_svistunov.lesson_6.level_4;
+
+import java.util.Scanner;
+
+public class Task_15 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+        System.out.println("Please, enter number: ");
+        int number = scanner.nextInt();
+    }
+}
+}
