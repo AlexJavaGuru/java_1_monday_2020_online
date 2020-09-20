a47f62efe3949485d6365345caf0f6e2a3e7608a
regina <Shjhi91.reg>
2020-09-20 21:41:30 +0300

    Lesson 6 level 3  homework

---
+++ src/student_regina_svistunov/lesson_6/level_3/ArrayUtil.java
@@ -0,0 +1,91 @@
+package student_regina_svistunov.lesson_6.level_3;
+
+import java.util.Random;
+
+public class ArrayUtil {
+
+        public int[] createArray(int arrayLength) {
+            return new int[arrayLength];
+        }
+
+        public void fillArrayWithRandomNumbers(int[] array) {
+
+            Random random = new Random();
+
+            for (int i=0; i < array.length; i++) {
+                array[i] = random.nextInt(50);
+            }
+        }
+        public int findMaxNumber(int[] array) {
+            int maxNumber = array[0];
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] > maxNumber) {
+                    maxNumber = array[i];
+                }
+            }  return maxNumber;
+        }
+        public int findMinNumber (int[] array) {
+            int minNumber = array[0];
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] < minNumber) {
+                    minNumber = array[i];
+                }
+            }
+            return minNumber;
+        }
+
+    public int findNumber(int[] newArray, int findNumber) {
+            int number = 0;
+        for (int i=0; i<newArray.length; i++) {
+            if (newArray[i] == findNumber) {
+                number = newArray[i];
+            } else {
+                findNumber = findNumber;
+            }
+        }
+        return number;
+    }
+    public int countNumber (int[] newArray, int countNumber) {
+            int number =0;
+            for (int i =0; i<newArray.length; i++) {
+                if (newArray[i] == countNumber) {
+                    number++;
+                }
+            }
+            return number;
+    }
+
+    public int replace(int[] array, int numberToReplace, int newNumber) {
+            int number = 0;
+            for (int i=0; i<array.length; i++) {
+                if (array[i] == numberToReplace) {
+                    array[i] = newNumber;
+                    number = i;
+                    i = array.length;
+                }
+            }
+            return number;
+    }
+    public int replaceAll(int[] array, int numberToReplace, int newNumber) {
+            int number = 0;
+            for (int i=0; i<array.length; i++) {
+                if (array[i] == numberToReplace) {
+                    array[i] = newNumber;
+                    number++;
+                }
+            }
+            return number;
+    }
+    public int[] reverse(int[] array) {
+            int number;
+            for(int i=0; i<array.length; i++) {
+                number = array[array.length -1 - i];
+                array[i] = number;
+            }
+            return array;
+    }
+    }
+
+
+
+
