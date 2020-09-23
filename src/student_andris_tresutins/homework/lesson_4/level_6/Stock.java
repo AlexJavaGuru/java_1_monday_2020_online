1ef2105ee981ec19682b03bb37e2bf943d24f3cc
Darxney <tresutinsandris@gmail.com>
2020-09-23 16:00:03 +0300

    Lesson 4 level 6

--- src/student_andris_tresutins/homework/lesson_4/level_6/Stock.java
+++ src/student_andris_tresutins/homework/lesson_4/level_6/Stock.java
@@ -14,12 +14,35 @@ package student_andris_tresutins.homework.lesson_4.level_6;
 
     }
 
-    public void updatePrice(int currentPrice, int maxPrice, int minPrice){
-        this.currentPrice = currentPrice;
-        this.maxPrice = maxPrice;
-        this.minPrice = minPrice;
+     public void updatePrice(int newPrice) {
 
-    }
+         currentPrice = newPrice;
+
+         if (newPrice > maxPrice) {
+             maxPrice = newPrice;
+         }
+         else if (newPrice < minPrice) {
+             minPrice = newPrice;
+         }
+     }
+
+     public String getName(){
+        return this.companyName = companyName;
+
+     }
+     public int getCurentPrice(){
+        return this.currentPrice = currentPrice;
+
+     }
+     public int getMax(){
+         return this.maxPrice = maxPrice;
+
+     }
+     public int getMin(){
+         return this.minPrice = minPrice;
+
+     }
+
 
     public void getPriceInformation(){
         System.out.println("Company = " + companyName + ", Current Price = " + currentPrice + ", Min Price = " + minPrice + ", Max price = " + maxPrice);
--- src/student_andris_tresutins/homework/lesson_4/level_6/StockDemo.java
+++ src/student_andris_tresutins/homework/lesson_4/level_6/StockDemo.java
@@ -1,11 +1,14 @@
 package student_andris_tresutins.homework.lesson_4.level_6;
 
 class StockDemo {
+
     public static void main(String[] args) {
         Stock stock = new Stock("Tesla",10);
         stock.getPriceInformation();
 
-        stock.updatePrice(15,24,8);
+        stock.updatePrice(18);//max
+        stock.updatePrice(9);//min
+        stock.updatePrice(12);//current
         stock.getPriceInformation();
 
     }
