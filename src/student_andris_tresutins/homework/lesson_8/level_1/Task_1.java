61bd4d868081a303659686363934e07803a99f28
Darxney <tresutinsandris@gmail.com>
2020-09-25 12:42:33 +0300

    //incomplete

---
+++ src/student_andris_tresutins/homework/lesson_8/level_1/Task_1.java
@@ -0,0 +1,7 @@
+package student_andris_tresutins.homework.lesson_8.level_1;
+
+class Task_1 {
+    Task_1(){
+        System.out.println("Default constructor");
+    }
+}
---
+++ src/student_andris_tresutins/homework/lesson_8/level_1/Task_1Demo.java
@@ -0,0 +1,4 @@
+package student_andris_tresutins.homework.lesson_8.level_1;
+
+public class Task_1Demo {
+}
