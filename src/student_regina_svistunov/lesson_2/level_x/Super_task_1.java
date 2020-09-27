f0d48d597d1b01cb920ae168f04e52761e160881
regina <Shjhi91.reg>
2020-09-27 12:33:10 +0300

    Lesson 2 leve super task

---
+++ src/student_regina_svistunov/lesson_2/level_x/Super_task_1.java
@@ -0,0 +1,35 @@
+package student_regina_svistunov.lesson_2.level_x;
+
+public class Super_task_1 {
+}
+
+class Book{
+
+}
+class BankAccount {
+
+}
+class SystemUnit{
+
+}
+class LoginService{
+
+}
+class BankTransaction{
+
+}
+class Task_1{
+
+}
+
+
+//    Дайте правильные с точки зрения Java кода названия классам:
+//
+//        book
+//        Bankaccount
+//        systemUnit
+//        Login_Service
+//        banktransaction
+//        task1
+//
+//        Создайте эти классы с корректными с точки зрения Java кода назнаниями.
