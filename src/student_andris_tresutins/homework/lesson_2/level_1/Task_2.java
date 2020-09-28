3760b2435e315ba30ec7ed38bb32fa73887afbf4
Darxney <tresutinsandris@gmail.com>
2020-09-28 11:34:02 +0300

    //incomplete

--- src/student_andris_tresutins/homework/lesson_2/level_1/Task_2.java
+++ src/student_andris_tresutins/homework/lesson_2/level_1/Task_2.java
@@ -7,20 +7,23 @@ import java.util.Scanner;
 
 @CodeReview(approved = false)
 @CodeReviewComment(comment = "Нужно исправить формат в классе.")
- class Task_2 {
+class Task_2 {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-            System.out.println( "enter number 1");
+        System.out.println("enter number 1");
         double num1 = input.nextDouble();
-            System.out.println( "enter number 2");
+
+        System.out.println("enter number 2");
         double num2 = input.nextDouble();
-            System.out.print( "add: ");
-            System.out.println(num1 + num2 );
-            System.out.print( "subtract: ");
-            System.out.println(num1 - num2 );
-            System.out.print( "multiply: ");
-            System.out.println(num1 * num2 );
-            System.out.print( "divide: ");
-            System.out.println(num1 / num2 );
+        
+        System.out.print("add: ");
+        System.out.println(num1 + num2);
+        System.out.print("subtract: ");
+        System.out.println(num1 - num2);
+        System.out.print("multiply: ");
+        System.out.println(num1 * num2);
+        System.out.print("divide: ");
+        System.out.println(num1 / num2);
     }
 }
--- src/student_andris_tresutins/homework/lesson_8/level_1/Task_1.java
+++ src/student_andris_tresutins/homework/lesson_8/level_1/Task_1.java
@@ -1,6 +1,8 @@
 package student_andris_tresutins.homework.lesson_8.level_1;
 
 class Task_1 {
+
+
     Task_1(){
         System.out.println("Default constructor");
     }
