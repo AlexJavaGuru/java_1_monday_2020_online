5f65c666dbc791c4e50e94044e49dac4ac0d71ee
regina <Shjhi91.reg>
2020-10-04 14:36:26 +0300

    Lesson 8 level 2

---
+++ src/student_regina_svistunov/lesson_8/level_2/Task_6/Father.java
@@ -0,0 +1,4 @@
+package student_regina_svistunov.lesson_8.level_2.Task_6;
+
+public class Father {
+}
---
+++ src/student_regina_svistunov/lesson_8/level_2/Task_9/Player.java
@@ -0,0 +1,10 @@
+package student_regina_svistunov.lesson_8.level_2.Task_9;
+
+public class Player {
+
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+}
