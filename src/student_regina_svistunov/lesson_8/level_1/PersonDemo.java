29798ae09c874795578c2e5a402fe6405d53eae5
regina <Shjhi91.reg>
2020-10-04 13:54:57 +0300

    Lesson 8 level 1

---
+++ src/student_regina_svistunov/lesson_8/level_1/PersonDemo.java
@@ -0,0 +1,11 @@
+package student_regina_svistunov.lesson_8.level_1;
+
+class PersonDemo {
+
+    public static void main(String[] args) {
+
+        Person person = new Person("Regina", 29, "Estonia, Tallinn");
+
+    }
+
+}
