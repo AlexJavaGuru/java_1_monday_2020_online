5acaea05e5fc0b83678c9f6889058830041ec79a
regina <Shjhi91.reg>
2020-10-04 17:52:29 +0300

    Lesson 8 level 3

---
+++ src/student_regina_svistunov/lesson_8/level_3/Task_12/Task_12.java
@@ -0,0 +1,15 @@
+package student_regina_svistunov.lesson_8.level_3.Task_12;
+
+//Исправьте код программы так, что бы он компилировался.
+//        Реализовывать метод canReceiveLoan() нельзя.
+//
+//class LoanSystem {
+//
+//    abstract boolean canReceiveLoan(Human human);
+//}
+
+abstract class LoanSystem {
+
+        abstract boolean canReceiveLoan(Human human);
+    }
+
---
+++ src/student_regina_svistunov/lesson_8/level_3/Task_13/Dog.java
@@ -0,0 +1,8 @@
+package student_regina_svistunov.lesson_8.level_3.Task_13;
+
+//Придумайте и создайте абстрактный класс и его потомков.
+
+public abstract class Dog {
+
+    abstract void voice();
+}
