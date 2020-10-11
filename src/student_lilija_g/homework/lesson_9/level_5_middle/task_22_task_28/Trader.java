fa591e546ceb19ddca3ad0283deaccd3958b4808
lilija <lgj@inbox.lv>
2020-10-11 21:43:27 +0300

    lesson_9 level_5_middle task_22_task_28

---
+++ src/student_lilija_g/homework/lesson_9/level_5_middle/task_22_task_28/Trader.java
@@ -0,0 +1,26 @@
+package student_lilija_g.homework.lesson_9.level_5_middle.task_22_task_28;
+
+class Trader {
+
+    private String fullName;
+    private String city;
+    private String country;
+
+    public Trader(String fullName, String city, String country) {
+        this.fullName = fullName;
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
