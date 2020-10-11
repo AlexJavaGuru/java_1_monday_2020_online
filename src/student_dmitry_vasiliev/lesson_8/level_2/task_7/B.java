b6dbda0613ffe1b84d246ebb5dbf37849d00276e
Dmbej <dmbej@hot.ee>
2020-10-11 11:11:26 +0300

    lesson_7.level_1_Homework_Task_7-8

---
+++ src/student_dmitry_vasiliev/lesson_8/level_2/task_7/B.java
@@ -0,0 +1,11 @@
+package student_dmitry_vasiliev.lesson_8.level_2.task_7;
+
+import student_dmitry_vasiliev.lesson_8.level_2.task_7.A;
+
+class B extends A {
+
+    B() {
+        super();
+        System.out.println("Constructor without arguments of class B");
+    }
+}
