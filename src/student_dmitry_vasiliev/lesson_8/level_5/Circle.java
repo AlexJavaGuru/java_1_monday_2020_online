9538e4f4a4f7059bc202adacb9354460b1eb5e21
Dmbej <dmbej@hot.ee>
2020-10-18 20:47:33 +0300

    lesson_8.level_5

---
+++ src/student_dmitry_vasiliev/lesson_8/level_5/Circle.java
@@ -0,0 +1,30 @@
+package student_dmitry_vasiliev.lesson_8.level_5;
+
+class Circle extends Shape {
+
+    private String title = "Circle";
+    private double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+        //this.title = title;
+    }
+
+    @Override
+    String name() {
+        String name = title;
+        return name;
+    }
+
+    @Override
+    double calculateArea() {
+        double area = Math.PI * (radius * radius);
+        return area;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        double perimetr = 2 * Math.PI * radius;
+        return perimetr;
+    }
+}
---
+++ src/student_dmitry_vasiliev/lesson_8/level_5/Triangle.java
@@ -0,0 +1,29 @@
+package student_dmitry_vasiliev.lesson_8.level_5;
+
+class Triangle extends student_dmitry_vasiliev.lesson_8.level_5.Shape {
+
+    private double side;
+    private String title = "Triangle";
+
+    Triangle(double side) {
+        this.side = side;
+    }
+
+    @Override
+    String name() {
+        String name = title;
+        return name;
+    }
+
+    @Override
+    double calculateArea() {
+        double area = ((side * side) * Math.sqrt(3)) / 4;
+        return area;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        double perimetr = 3 * side;
+        return perimetr;
+    }
+}
