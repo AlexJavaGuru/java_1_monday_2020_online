d085f21efb38e09003ec41496cb2f589a9456fdb
Dmbej <dmbej@hot.ee>
2020-10-20 17:03:00 +0300

    lesson_9.level_1

---
+++ src/student_dmitry_vasiliev/lesson_9/level_1/Book.java
@@ -0,0 +1,11 @@
+package student_dmitry_vasiliev.lesson_9.level_1;
+
+// - Task_6
+// Исправьте код класса Book так, что бы он соответствовал принципу инкапсуляции.
+
+class Book {
+
+    private String title;
+    private String author;
+
+}
---
+++ src/student_dmitry_vasiliev/lesson_9/level_1/PrivateClass.java
@@ -0,0 +1,12 @@
+package student_dmitry_vasiliev.lesson_9.level_1;
+
+// - Task_7
+// Исправьте код класса PrivateClass так, что бы он компилировался.
+
+class PrivateClass {
+
+    private String title;
+    private String author;
+
+
+}
