c5b7da1ec7dac8c18e3f67d89d9f9e5437314efb
regina <Shjhi91.reg>
2020-11-01 15:25:55 +0200

    Lesson 4 level 3-4 Super tasks

---
+++ src/main/java/student_regina_svistunov/lesson_4/level_x/super_task_3/CalculatorTest.java
@@ -0,0 +1,57 @@
+package student_regina_svistunov.lesson_4.level_x.super_task_3;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    public static void main(String[] args) {
+        CalculatorTest calculatorTest = new CalculatorTest();
+
+        calculatorTest.sumTest();
+        calculatorTest.subTest();
+        calculatorTest.mulTest();
+        calculatorTest.divTest();
+        calculatorTest.isEvenTest1();
+        calculatorTest.isEvenTest2();
+    }
+
+    void calcTest(int actualNumber, int expectedNumber, String testName){
+        if (actualNumber == expectedNumber) {
+            System.out.println(testName + " test is PASSED");
+        } else {
+            System.out.println(testName + " test is FAILED");
+        }
+    }
+
+    void evenTest(boolean actualNumber, String testName) {
+        if (actualNumber) {
+            System.out.println(testName + " test is PASSED");
+        } else {
+            System.out.printf(testName + " test is FAILED");
+        }
+    }
+
+    public void sumTest() {
+        calcTest(calculator.sum(10,15),25," Sum");
+    }
+
+    public void subTest() {
+        calcTest(calculator.sub(10,3),7," Sub");
+    }
+
+    public void mulTest() {
+        calcTest(calculator.mul(3,4),12, " Mul");
+    }
+
+    public void divTest() {
+        calcTest(calculator.div(9,3),3, " Div");
+    }
+
+    public void isEvenTest1() {
+        evenTest(calculator.isEven(34), " Even");
+    }
+
+    public void isEvenTest2() {
+        evenTest(calculator.isEven(33)," Even" );
+    }
+}
---
+++ src/main/java/student_regina_svistunov/lesson_4/level_x/super_task_4/LightColorDetector.java
@@ -0,0 +1,39 @@
+package student_regina_svistunov.lesson_4.level_x.super_task_4;
+
+//Дан класс LightColorDetector и его тест LightColorDetectorTest.
+//        Найдите способ упростить тесты в LightColorDetectorTest.
+//
+//        Тесты тоже должны быть написаны как можно лучше и качественнее!
+//        Дублирование кода - это почти всегда плохая идея в программировании.
+//        Поэтому от дублирования кода нужно избавляться.
+//
+//        Подсказка: вынесите в отдельный метод проверку
+//        результата и замените её вызовом метода в каждом тесте.
+//
+//        Инстанция класса LightColorDetector не имеет состояния,
+//        она stateless. Найдите в интернете, что такое stateless класс
+//        в программировании. Найдите способ использовать одну
+//        инстанцию класса LightColorDetector для всех тестов.
+
+public class LightColorDetector {
+
+    public String detect(int wavelength) {
+
+        if (wavelength >= 380 && wavelength <= 449) {
+            return "Violet";
+        } else if (wavelength >= 450 && wavelength <= 494) {
+            return "Blue";
+        } else if (wavelength >= 495 && wavelength <= 569) {
+            return "Green";
+        } else if (wavelength >= 570 && wavelength <= 589) {
+            return "Yellow";
+        } else if (wavelength >= 590 && wavelength <= 619) {
+            return "Orange";
+        } else if (wavelength >= 620 && wavelength <= 750) {
+            return "Red";
+        } else {
+            return "Invisible Light";
+        }
+    }
+}
+
